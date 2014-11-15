4471ada2f5b83e367951dcef5cb333e521be32c6
Noahke <meltypavement@gmail.com>
2014-11-15 15:50:20 -0500

    Update org model object

--- app/models/Org.java
+++ app/models/Org.java
@@ -1,41 +1,82 @@
 package models;
-import java.util.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-public class Org {
-	public String name;
-	public ArrayList users;
-	public String contact;
-	public ArrayList events;
+import play.Logger;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name="organizations")
+public class Org extends Model {
 	
-	public Org()	{
-		users = new ArrayList<Object>();
-		events = new ArrayList<Object>();
+	private static final long serialVersionUID = 436818248219553589L;
+
+	@Id
+	@SequenceGenerator(name="orgs_seq", sequenceName="orgs_id_seq", allocationSize=0)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="orgs_seq")
+	private int id;
+	
+	@Required
+	private String name;
+	
+	@Required
+	private User owner;
+	
+	@Required
+	private String contact;
+	
+	@Required
+	private String url;
+			
+	public static void createShoes(Org org) {
+		Logger.debug(org.toString());
+		
+		org.save();
 	}
-	
-	public String getName()	{
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
 		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 	
-	public String setName(String newName)	{
-		name = newName;
-		return name;
-	}
-	public ArrayList getUsers()	{
-		return users;
-	}
-	
-	public ArrayList addUser(Object newUser)	{
-		users.add(newUser);
-		return users;
-	}
-	
-	public ArrayList getEvents(){
-		return events;
-	}
-	
-	public ArrayList addEvent(Object newEvent){
-		events.add(newEvent);
-		return events;
-	}
-	
-	}
+}
