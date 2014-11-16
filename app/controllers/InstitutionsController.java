a2c482cf2ac10221cd87d31c84b6399579dea574
Noahke <meltypavement@gmail.com>
2014-11-16 08:14:05 -0500

    Added info pages for models; refactored a few method names

--- app/controllers/InstitutionsController.java
+++ app/controllers/InstitutionsController.java
@@ -3,16 +3,22 @@ package controllers;
 import java.util.LinkedList;
 import java.util.List;
 
+import models.Event;
 import models.Institution;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class InstitutionsController extends Application {
 	
-	public static Result foo() {
+	public static Result allInstitutions() {
 		List<Institution> institutions = new LinkedList<Institution>();
 		institutions = Institution.find.all();
         return ok(views.html.institutions.render(institutions));
     }
+	
+	public static Result institutionInfo(Integer id) {
+		Institution institution = new Institution();
+		return ok(views.html.institutionInfo.render(institution));
+	}
 
 }
--- app/controllers/OrgsController.java
+++ app/controllers/OrgsController.java
@@ -10,10 +10,15 @@ import play.mvc.Result;
 
 public class OrgsController extends Application {
 	
-	public static Result foo() {
+	public static Result allOrgs() {
 		List<Org> orgs = new LinkedList<Org>();
 		orgs = Org.find.all();
         return ok(views.html.orgs.render(orgs));
     }
+	
+	public static Result orgInfo(Integer id) {
+		Org org = new Org();
+		return ok(views.html.orgInfo.render(org));
+	}
 
 }
