69fdc9bc82a21fb01a42cd19dcd30606ab5b1fec
Noahke <meltypavement@gmail.com>
2014-11-16 11:00:20 -0500

    Pass model to institution page

--- app/controllers/InstitutionsController.java
+++ app/controllers/InstitutionsController.java
@@ -3,9 +3,7 @@ package controllers;
 import java.util.LinkedList;
 import java.util.List;
 
-import models.Event;
 import models.Institution;
-import play.mvc.Controller;
 import play.mvc.Result;
 
 public class InstitutionsController extends Application {
@@ -17,8 +15,11 @@ public class InstitutionsController extends Application {
     }
 	
 	public static Result institutionInfo(Integer id) {
-		Institution institution = new Institution();
-		return ok(views.html.institutionInfo.render(institution));
+		Institution inst = Institution.find.byId(id);
+		if (inst == null) {
+			return ok(views.html.errorPage.render("Sorry, that institution doesn't exist!!"));
+		}
+		return ok(views.html.institutionInfo.render(inst));
 	}
 
 }
