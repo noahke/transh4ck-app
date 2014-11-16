a9be37f03c54f8b922a7d31b94dd521f5a41fd3d
Sparrow French <khayankh+spam@gmail.com>
2014-11-15 20:28:36 -0500

    Orgs Display Still Doesn't Work

--- app/controllers/OrgsController.java
+++ app/controllers/OrgsController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.LinkedList;
 import java.util.List;
 
+import models.Institution;
 import models.Org;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -11,6 +12,7 @@ public class OrgsController extends Application {
 	
 	public static Result foo() {
 		List<Org> orgs = new LinkedList<Org>();
+		orgs = Org.find.all();
         return ok(views.html.orgs.render(orgs));
     }
 
