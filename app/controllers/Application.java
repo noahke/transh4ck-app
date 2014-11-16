66a1afac96ff7dbcd81cc51d1af998546e33b6f7
stickmoon <eh11@hampshire.edu>
2014-11-15 20:26:47 -0500

    Static HTML for main page, index method in application control no longer null.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,11 +8,13 @@ import models.Org;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import views.html.*;
+
 public class Application extends Controller {
 	
-	public static Result index() {
-		return null;
-	}
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
 	
 	public static int generateId() {
 		Random rand = new Random();
